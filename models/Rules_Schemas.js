8e5898715bc9a4bd24e948019845bd1dfe4175cd
evg31337 <evg31337@gmail.com>
2015-02-12 23:36:22 -0500

    New Schema

--- models/Rules_Schemas.js
+++ models/Rules_Schemas.js
@@ -1,15 +1,8 @@
 var mongoose = require('mongoose');
 
 var RulesSchema = new mongoose.Schema({
-  title: String,
-  category: String,
-  titleSubtext: String,
-  image: String,
-  quickDescription: String,
-  mainText: String,
+  name: String,
+  description: String,
   date: { type: Date, default: Date.now},
 });
-
-
-
 module.exports = mongoose.model('API_Rules', RulesSchema);

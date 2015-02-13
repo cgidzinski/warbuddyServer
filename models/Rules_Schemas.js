024196d8cce7cc33963d2a6249915c32e7c29b7a
evg31337 <evg31337@gmail.com>
2015-02-13 00:18:44 -0500

    Renamed var

--- models/Rules_Schemas.js
+++ models/Rules_Schemas.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 var RulesSchema = new mongoose.Schema({
   name: String,
   description: String,
-  date: { type: Date, default: Date.now},
+  tdate: { type: Date, default: Date.now},
 });
 module.exports = mongoose.model('API_Rules', RulesSchema);

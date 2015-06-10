bd7949b983d88f0f7bb42d808762f2a2f96b9a91
Colin Gidzinski <evg31337@gmail.com>
2015-06-10 14:03:33 -0400

    New local source

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,98 +4,74 @@
 //Unit Movement
 angular.module('WarBuddy.controllers', ['ngStorage']).controller('HomeCtrl', function ($scope, $ionicModal, $localStorage, $stateParams, $state) {})
     //////////////////////////////////////////////////////////////////////
-    .controller('UnitListCtrl', function ($scope, $http, $state, $ionicModal)
-    {
+    .controller('UnitListCtrl', function ($scope, $http, $state, $ionicModal) {
         console.log("Unit List Page Loaded");
         unitGet();
-        $ionicModal.fromTemplateUrl('templates/modal-Unit.html',
-        {
+        $ionicModal.fromTemplateUrl('templates/modal-Unit.html', {
             scope: $scope,
             animation: 'slide-in-up'
-        }).then(function (modal)
-        {
+        }).then(function (modal) {
             $scope.modal = modal
         })
-        $scope.openModal = function (item)
-        {
-            if (item._id == undefined)
-            {
+        $scope.openModal = function (item) {
+            if (item._id == undefined) {
                 $scope.item = {};
-            }
-            else
-            {
+            } else {
                 $scope.item = item
             }
             $scope.modal.show()
         }
-        $scope.closeModal = function (item)
-        {
-            if (item._id == undefined)
-            {
+        $scope.closeModal = function (item) {
+            if (item._id == undefined) {
                 unitPost(item);
-            }
-            else
-            {
+            } else {
                 unitPut(item);
             }
             $scope.modal.hide();
         };
-        $scope.$on('$destroy', function ()
-        {
+        $scope.$on('$destroy', function () {
             $scope.modal.remove();
         });
-
-         $scope.unitDelete = function(item)
-        {
+        $scope.unitDelete = function (item) {
             $http.delete('http://evg31337.com:3001/API_Units/' + item._id).
-            success(function (data, status, headers, config)
-            {
+            success(function (data, status, headers, config) {
                 unitGet();
             });
         }
 
-        function unitPost(item)
-        {
+        function unitPost(item) {
             $http.post('http://evg31337.com:3001/API_Units/', item).
-            success(function (data, status, headers, config)
-            {
+            success(function (data, status, headers, config) {
                 console.log("added");
                 unitGet();
             });
         }
 
-        function unitPut(item)
-        {
+        function unitPut(item) {
             $http.put('http://evg31337.com:3001/API_Units/' + item._id, item).
-            success(function (data, status, headers, config)
-            {
+            success(function (data, status, headers, config) {
                 unitGet();
             });
         }
 
-        function unitGet()
-        {
+        function unitGet() {
             $http.get('http://evg31337.com:3001/API_Units').
-            success(function (data, status, headers, config)
-            {
+            success(function (data, status, headers, config) {
                 $scope.units = data;
             });
         }
     })
     ///////////////////////////
-    .controller('UnitCtrl', function ($scope, $http, $stateParams, $state)
-    {
+    .controller('UnitCtrl', function ($scope, $http, $stateParams, $state) {
         console.log("Unit Detail Page Loaded");
         $scope.unit = [];
         $scope.unit.rules = [];
         var id = $stateParams.Id;
         $http.get('http://evg31337.com:3001/API_Units/' + id).
-        success(function (data, status, headers, config)
-        {
+        success(function (data, status, headers, config) {
             //Success Log
             $scope.unit = data;
-            switch ($scope.unit.type)
-            {
+            switch ($scope.unit.type) {
             case "Infantry":
                 $scope.unit.move = 'Movement: 6", Charge: D6';
                 break;
@@ -144,8 +120,7 @@ angular.module('WarBuddy.controllers', ['ngStorage']).controller('HomeCtrl', fun
             }
             console.log(data);
         }).
-        error(function (data, status, headers, config)
-        {
+        error(function (data, status, headers, config) {
             //Fail Log
             console.log("Connection Failed");
             console.log(data + status + headers);
@@ -153,35 +128,26 @@ angular.module('WarBuddy.controllers', ['ngStorage']).controller('HomeCtrl', fun
         $scope.ruleList = [];
         $scope.weaponList = [];
         $http.get('http://evg31337.com:3001/API_Weapons/').
-        success(function (data, status, headers, config)
-        {
+        success(function (data, status, headers, config) {
             //Success Log
-            $scope.unit.weapons.split(",").forEach(function (uWeapon)
-            {
-                data.forEach(function (rWeapon)
-                {
-                    if (uWeapon == rWeapon.name)
-                    {
+            $scope.unit.weapons.split(",").forEach(function (uWeapon) {
+                data.forEach(function (rWeapon) {
+                    if (uWeapon == rWeapon.name) {
                         console.log(rWeapon);
                         $scope.weaponList.push(rWeapon);
                         $http.get('http://evg31337.com:3001/API_Rules/').
-                        success(function (data, status, headers, config)
-                        {
+                        success(function (data, status, headers, config) {
                             //Success Log
-                            rWeapon.rules.split(",").forEach(function (wRule)
-                            {
-                                data.forEach(function (rRule)
-                                {
-                                    if (wRule == rRule.name)
-                                    {
+                            rWeapon.rules.split(",").forEach(function (wRule) {
+                                data.forEach(function (rRule) {
+                                    if (wRule == rRule.name) {
                                         console.log(rRule);
                                         $scope.ruleList.push(rRule);
                                     }
                                 });
                             });
                         }).
-                        error(function (data, status, headers, config)
-                        {
+                        error(function (data, status, headers, config) {
                             //Fail Log
                             console.log("Connection Failed");
                             console.log(data + status + headers);
@@ -190,135 +156,196 @@ angular.module('WarBuddy.controllers', ['ngStorage']).controller('HomeCtrl', fun
                 });
             });
         }).
-        error(function (data, status, headers, config)
-        {
+        error(function (data, status, headers, config) {
             //Fail Log
             console.log("Connection Failed");
             console.log(data + status + headers);
         });
         $http.get('http://evg31337.com:3001/API_Rules/').
-        success(function (data, status, headers, config)
-        {
+        success(function (data, status, headers, config) {
             //Success Log
-            $scope.unit.rules.split(",").forEach(function (uRule)
-            {
-                data.forEach(function (rRule)
-                {
-                    if (uRule == rRule.name)
-                    {
+            $scope.unit.rules.split(",").forEach(function (uRule) {
+                data.forEach(function (rRule) {
+                    if (uRule == rRule.name) {
                         console.log(rRule);
                         $scope.ruleList.push(rRule);
                     }
                 });
             });
         }).
-        error(function (data, status, headers, config)
-        {
+        error(function (data, status, headers, config) {
             //Fail Log
             console.log("Connection Failed");
             console.log(data + status + headers);
         });
     })
     //////////////////////////////////////////////////////////////////////
-    .controller('WeaponListCtrl', function ($scope, $http, $state)
-    {
+    .controller('WeaponListCtrl', function ($scope, $http, $state, $ionicModal) {
         console.log("Weapon List Page Loaded");
-        $http.get('http://evg31337.com:3001/API_Weapons').
-        success(function (data, status, headers, config)
-        {
-            //Success Log
-            $scope.weapons = data;
-            console.log(data);
-        }).
-        error(function (data, status, headers, config)
-        {
-            //Fail Log
-            console.log("Connection Failed");
-            console.log(data + status + headers);
+       weaponGet();
+        $ionicModal.fromTemplateUrl('templates/modal-Weapon.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function (modal) {
+            $scope.modal = modal
+        })
+        $scope.openModal = function (item) {
+            if (item._id == undefined) {
+                $scope.item = {};
+            } else {
+                $scope.item = item
+            }
+            $scope.modal.show()
+        }
+        $scope.closeModal = function (item) {
+            if (item._id == undefined) {
+                weaponPost(item);
+            } else {
+                weaponPut(item);
+            }
+            $scope.modal.hide();
+        };
+        $scope.$on('$destroy', function () {
+            $scope.modal.remove();
         });
+        $scope.weaponDelete = function (item) {
+            $http.delete('http://evg31337.com:3001/API_weapons/' + item._id).
+            success(function (data, status, headers, config) {
+                weaponGet();
+            });
+        }
+
+        function weaponPost(item) {
+            $http.post('http://evg31337.com:3001/API_weapons/', item).
+            success(function (data, status, headers, config) {
+                console.log("added");
+                weaponGet();
+            });
+        }
+
+        function weaponPut(item) {
+            $http.put('http://evg31337.com:3001/API_weapons/' + item._id, item).
+            success(function (data, status, headers, config) {
+                weaponGet();
+            });
+        }
+
+        function weaponGet() {
+            $http.get('http://evg31337.com:3001/API_weapons').
+            success(function (data, status, headers, config) {
+                $scope.weapons = data;
+            });
+        }
     })
     ///////////////////////////
-    .controller('WeaponCtrl', function ($scope, $http, $stateParams)
-    {
+    .controller('WeaponCtrl', function ($scope, $http, $stateParams) {
         console.log("Weapon Detail Page Loaded");
         var id = $stateParams.Id;
         $http.get('http://evg31337.com:3001/API_Weapons/' + id).
-        success(function (data, status, headers, config)
-        {
+        success(function (data, status, headers, config) {
             //Success Log
             $scope.weapon = data;
             console.log(data);
         }).
-        error(function (data, status, headers, config)
-        {
+        error(function (data, status, headers, config) {
             //Fail Log
             console.log("Connection Failed");
             console.log(data + status + headers);
         });
         $scope.ruleList = [];
         $http.get('http://evg31337.com:3001/API_Rules/').
-        success(function (data, status, headers, config)
-        {
+        success(function (data, status, headers, config) {
             //Success Log
-            $scope.weapon.rules.split(",").forEach(function (wRule)
-            {
-                data.forEach(function (rRule)
-                {
-                    if (wRule == rRule.name)
-                    {
+            $scope.weapon.rules.split(",").forEach(function (wRule) {
+                data.forEach(function (rRule) {
+                    if (wRule == rRule.name) {
                         console.log(rRule);
                         $scope.ruleList.push(rRule);
                     }
                 });
             });
         }).
-        error(function (data, status, headers, config)
-        {
+        error(function (data, status, headers, config) {
             //Fail Log
             console.log("Connection Failed");
             console.log(data + status + headers);
         });
     })
     //////////////////////////////////////////////////////////////////////
-    .controller('RuleListCtrl', function ($scope, $http, $state)
-    {
+    .controller('RuleListCtrl', function ($scope, $http, $state, $ionicModal) {
         console.log("Rule List Page Loaded");
-        $http.get('http://evg31337.com:3001/API_Rules').
-        success(function (data, status, headers, config)
-        {
-            //Success Log
-            $scope.rules = data;
-            console.log(data);
-        }).
-        error(function (data, status, headers, config)
-        {
-            //Fail Log
-            console.log("Connection Failed");
-            console.log(data + status + headers);
+        ruleGet();
+        $ionicModal.fromTemplateUrl('templates/modal-Rule.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function (modal) {
+            $scope.modal = modal
+        })
+        $scope.openModal = function (item) {
+            if (item._id == undefined) {
+                $scope.item = {};
+            } else {
+                $scope.item = item
+            }
+            $scope.modal.show()
+        }
+        $scope.closeModal = function (item) {
+            if (item._id == undefined) {
+                rulePost(item);
+            } else {
+                rulePut(item);
+            }
+            $scope.modal.hide();
+        };
+        $scope.$on('$destroy', function () {
+            $scope.modal.remove();
         });
+        $scope.ruleDelete = function (item) {
+            $http.delete('http://evg31337.com:3001/API_Rules/' + item._id).
+            success(function (data, status, headers, config) {
+                ruleGet();
+            });
+        }
+
+        function rulePost(item) {
+            $http.post('http://evg31337.com:3001/API_Rules/', item).
+            success(function (data, status, headers, config) {
+                console.log("added");
+                ruleGet();
+            });
+        }
+
+        function rulePut(item) {
+            $http.put('http://evg31337.com:3001/API_Rules/' + item._id, item).
+            success(function (data, status, headers, config) {
+                ruleGet();
+            });
+        }
+
+        function ruleGet() {
+            $http.get('http://evg31337.com:3001/API_Rules').
+            success(function (data, status, headers, config) {
+                $scope.rules = data;
+            });
+        }
     })
     //////////////////////////////////////////////////////////////////////
-    .controller('ChartCtrl', function ($scope, $http, $state)
-    {
+    .controller('ChartCtrl', function ($scope, $http, $state) {
         console.log("Chart Page Loaded");
         $scope.tohitroll = "";
         $scope.towoundroll = "";
-        $scope.setActivea = function (type)
-        {
+        $scope.setActivea = function (type) {
             $scope.activea = type;
             calcstats();
         };
-        $scope.isActivea = function (type)
-        {
+        $scope.isActivea = function (type) {
             return type === $scope.activea;
         };
-        $scope.setActiveb = function (type)
-        {
+        $scope.setActiveb = function (type) {
             $scope.activeb = type;
             calcstats();
         };
-        $scope.isActiveb = function (type)
-        {
+        $scope.isActiveb = function (type) {
             return type === $scope.activeb;
         };
         $scope.tohitchart = [
@@ -346,26 +373,22 @@ angular.module('WarBuddy.controllers', ['ngStorage']).controller('HomeCtrl', fun
             ['2+', '2+', '2+', '2+', '2+', '2+', '2+', '2+', '3+', '4+']
         ];
 
-        function calcstats()
-        {
+        function calcstats() {
             $scope.tohitroll = $scope.tohitchart[$scope.activea][$scope.activeb]
             $scope.towoundroll = $scope.towoundchart[$scope.activea][$scope.activeb]
         }
     })
     ///////////////////////////
-    .controller('RuleCtrl', function ($scope, $http, $stateParams)
-    {
+    .controller('RuleCtrl', function ($scope, $http, $stateParams) {
         console.log("Rule Detail Page Loaded");
         var id = $stateParams.Id;
         $http.get('http://evg31337.com:3001/API_Rules/' + id).
-        success(function (data, status, headers, config)
-        {
+        success(function (data, status, headers, config) {
             //Success Log
             $scope.rule = data;
             console.log(data);
         }).
-        error(function (data, status, headers, config)
-        {
+        error(function (data, status, headers, config) {
             //Fail Log
             console.log("Connection Failed");
             console.log(data + status + headers);
